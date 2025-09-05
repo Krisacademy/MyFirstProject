c59f4a6d596da459b3207f841ab314f22d047de3
cherkezyan <kris@gmail.com>
2025-09-05 11:35:51 +0300

    Point

--- MyFirstProject/FileName.cpp
+++ MyFirstProject/FileName.cpp
@@ -1,18 +1,33 @@
 #include<iostream>
 using namespace std;
-class Point
-{
-private:
-	int x;
-	int y;
-public:
-	Point()
-	{
 
-	}
+class Point // создание своего ТИПА!!!
+{
+private: // реализация объекта
+    int x; // поле объекта
+    int y; // поле объекта
+    char symbol; // поле объекта
+public:   // интерфейс объекта
+    void Print() // метод класса 
+    {
+        cout << symbol << "-> X: " << x << "\tY: " << y << endl;
+    }
+    void Init(char t, int _x, int _y) // метод класса 
+    {
+        symbol = t;
+        x = _x;
+        y = _y;
+    }
 };
-
-int main()
+int main() // клент
 {
-	cout << "Hello world!!\n";
+    Point a, b;
+    //cout << sizeof(a) << endl; // 12
+
+    a.Init('A', 10, -20);
+    a.Print();
+
+    b.Init('B', 5, 16);
+    b.Print();
+
 }
